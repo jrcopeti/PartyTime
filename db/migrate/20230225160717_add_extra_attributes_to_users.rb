9433faeab24c898fbd003dc03537a6833664fb95
Jose Copeti <jrcopeti@gmail.com>
2023-02-27 18:10:25 +0100

    migration in users

--- db/migrate/20230225160717_add_extra_attributes_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddExtraAttributesToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :first_name
-    add_column :users, :last_name
-    add_column :users, :nickname
-    add_column :users, :bio
-  end
-end
--- db/migrate/20230225203430_remove_attributes_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAttributesFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :first_name
-    remove_column :users, :last_name
-    remove_column :users, :nickname
-    remove_column :users, :bio
-  end
-end
