8b9543d1831540ae61b25e91dcd599d5360cddcc
Jose Copeti <jrcopeti@gmail.com>
2023-03-01 20:19:31 +0100

    fixing database on heroku

--- db/migrate/20230301102722_add_start_date_and_end_date_to_events.rb
+++ db/migrate/20230301102722_add_start_date_and_end_date_to_events.rb
@@ -1,9 +1,5 @@
 class AddStartDateAndEndDateToEvents < ActiveRecord::Migration[7.0]
   def change
-    remove_column :events, :start_date
-    remove_column :events, :end_date
-    remove_column :events, :start_time
-    remove_column :events, :end_time
     add_column :events, :start_date, :datetime
     add_column :events, :end_date, :datetime
   end
