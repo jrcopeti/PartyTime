5062eab5bb5f84602942998cb402a73bffbc6890
Jose Copeti <jrcopeti@gmail.com>
2023-03-02 11:40:19 +0100

    controllers and views done

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :venues, only: %i[index show] do
-    resources :events, except: :index
+    resources :events
   end
 
   resources :events, only: :index
