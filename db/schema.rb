e11ac2d854f4f505934fa2c88d0be5c372cbf0c7
Alex Kurtu <alex-kurtu@hotmail.com>
2023-03-04 17:10:43 +0100

    Created a beautiful Club Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_184946) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_144945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_184946) do
     t.datetime "updated_at", null: false
     t.datetime "start_date"
     t.datetime "end_date"
+    t.string "image_url"
     t.index ["user_id"], name: "index_events_on_user_id"
     t.index ["venue_id"], name: "index_events_on_venue_id"
   end
@@ -98,6 +97,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_184946) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_reviews_on_event_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "rsvps", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.boolean "attending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_rsvps_on_event_id"
+    t.index ["user_id"], name: "index_rsvps_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -141,4 +150,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_184946) do
   add_foreign_key "messages", "users"
   add_foreign_key "reviews", "events"
   add_foreign_key "reviews", "users"
+  add_foreign_key "rsvps", "events"
+  add_foreign_key "rsvps", "users"
 end
