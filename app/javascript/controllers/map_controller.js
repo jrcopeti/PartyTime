eb615a381f36cdc88ca14b884da7e288d5ece842
Yonatan Amir <79724865+Jonathanamir1@users.noreply.github.com>
2023-03-04 15:00:58 +0100

    layout changes to map page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/jonathanamir1/cletzfekq000m01ph88obppxk"
     })
     this.#addMarkersToMap()
   }
